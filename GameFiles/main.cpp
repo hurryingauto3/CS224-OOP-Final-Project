430798c04ec2a079e0e107aa3d9461a131f6d7ea
Ali Hamza <51835003+hurryingauto3@users.noreply.github.com>
2019-11-02 15:01:15 +0500

    SDL Included

--- GameFiles/main.cpp
+++ GameFiles/main.cpp
@@ -1,3 +1,20 @@
+/*This source code copyrighted by Lazy Foo' Productions (2004-2015)
+  and may not be redistributed without written permission.*/
+
+//Using SDL, SDL_image, standard IO, math, and strings
+#ifdef __APPLE__
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+#else
+#include <SDL.h>
+#include <SDL_image.h>
+#endif
+
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <string>
+
 #include "Enemy.hpp"
 #include "Player.hpp"
 #include "Terrain.hpp"

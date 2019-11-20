1eb269e69c513a2dfdbbe136cf8ced1784fc8bc0
Ali Hamza <51835003+hurryingauto3@users.noreply.github.com>
2019-11-20 23:42:44 +0500

    Update Weapon.hpp

--- GameFiles/Weapon.hpp
+++ GameFiles/Weapon.hpp
@@ -1,3 +1,6 @@
+#include "Enemy.hpp"
+#include <cmath>
+
 class Weapon
 {
 private:
@@ -7,24 +10,60 @@ private:
     int magsize;
     float reload_time;
     char type;
+    bool gunpicked;
+    int xloc;
+    int yloc;
+    int bulletsfired;
 
 public:
-    char virtual weapontype()
+    int getx() { return this->xloc; }
+    int gety() { return this->yloc; }
+    void setdecayrate(float decrate) { this->decay_rate = decrate; }
+    void setattackpwr(float attack) { this->attackpwr = attack; }
+    float attackpwr() { return this->attackpwr; }
+    void appearonmap(int x, int y)
     {
-        char x = '0';
-        return x;
+        this->xloc = x;
+        this->yloc = y;
     }
-    void virtual attack() {}
-    int virtual bullets_left() {}
-    void virtual attack_decay() {}
-    void virtual reload() {}
-    void
+    void setweapontype(char type) { this->type = type; }
+    char weapontype() { return this->type; }
+    void fire()
+    {
+        this->bulletsfired++;
+    }
+    int bullets_left()
+    {
+        return this->magsize - this->bulletsfired;
+    }
+    float attackpower_decayed(Player A, Enemy B)
+    {
+        float tempattackpwr;
+        tempattackpwr = this->attackpwr - sqrt(pow((A.gety() - B.gety()), 2) + pow((A.getx() - B.getx()), 2)) * this->decay_rate;
+        return tempattackpwr;
+    }
+    void reload() {}
+    void setmagsize(int mag) { this->magsize = mag; }
+    int magsize() { return this->magsize; }
+    void pickgunup(Player A)
+    {
+        if (abs((A.gety() - this->gety()) < 5 | abs(A.getx() - this->getx()) < 5))
+        {
+            gunpicked = true;
+        }
+    }
+    bool gunpicked() { return this->gunpicked; }
 };
 
 class gun : public Weapon
 {
 private:
 public:
+    gun()
+    {
+        gun::setattackpwr(3.5);
+        gun::appearonmap(25, 25);
+    }
 };
 
 class minigun : public Weapon
